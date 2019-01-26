27fd2c16273f61a7d2d2f4fb24b3bc3be1f8a431
Ari-Mikko Penttilä <arimikko.penttila@gmail.com>
2019-01-26 15:12:55 +0200

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -57,4 +57,5 @@ def review(employer_id):
         status = request.method['status']
 
         date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
     return render_template('review.html', employer_id=employer_id)
